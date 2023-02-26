92dd6306db477ccba6d917adb7bb6ab7ad7d631a
Elvis <elviskftsang@gmail.com>
2023-02-25 22:23:51 -0500

    add loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,16 +4,18 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Container = styled.div`
     display: flex;
-    justify-content: center;
-    margin-top: 10%;
+    height: 50vh;
+    margin-top: 15%;
+    align-items: center;
+    flex-direction: column;
 `
 
 function Loading() {
     return (
         <Container>
-            <Spinner animation="border" variant="primary" />
+            <Spinner animation="border" variant="secondary" />
+            <div>Loading</div>
         </Container>
-        // <Spinner animation="border" variant="primary" />
     )
 }
 
