f0db4b800c0a9565f2448e7d33b7f23e15118f5c
Elvis <elviskftsang@gmail.com>
2023-02-25 21:19:38 -0500

    fix domain issue

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 		createNewListContent: false
 	})
 
-	const requestEndPoint = "https://elvistkf.synology.me:49255/api/grocery-list/"
+	const requestEndPoint = "https://api.elvistkf.synology.me/api/grocery-list/"
 
 	const [groceryList, setGroceryList] = useState(null);
 
